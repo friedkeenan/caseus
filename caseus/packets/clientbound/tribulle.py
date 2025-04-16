1f5852b5b975e8201f9e8ba45a2b9567e4536b43
friedkeenan <friedkeenan@protonmail.com>
2025-04-15 20:47:29 -0500

    Implement tribulle packets related to the tribe settings

--- caseus/packets/clientbound/tribulle.py
+++ caseus/packets/clientbound/tribulle.py
@@ -27,6 +27,10 @@ class ConnectedToCommunityPlatformPacket(ClientboundTribullePacket):
         greeting:       types.SignedLengthString
         house_map_code: types.Int
 
+    class RankInfo(pak.SubPacket):
+        name:        types.SignedLengthString
+        permissions: types.Int
+
     gender:    pak.Enum(types.Byte, enums.Gender)
     global_id: types.Int
 
@@ -34,9 +38,11 @@ class ConnectedToCommunityPlatformPacket(ClientboundTribullePacket):
     friends:   FriendInfo[types.Short]
     blacklist: types.SignedLengthString[types.Short]
 
-    tribe:             TribeInfo
-    tribe_rank:        types.SignedLengthString
-    tribe_permissions: types.Int
+    tribe_name:     types.SignedLengthString
+    tribe_id:       types.Int
+    greeting:       types.SignedLengthString
+    house_map_code: types.Int
+    tribe_rank:     RankInfo
 
 @public
 class WhisperPacket(ClientboundTribullePacket):
@@ -53,3 +59,41 @@ class ChangeTribeHouseMapResultPacket(FingerprintedClientboundTribullePacket):
 
     # TODO: Enum?
     result: types.Byte
+
+@public
+class GetTribeSettingsResultPacket(FingerprintedClientboundTribullePacket):
+    id = 109
+
+    # TODO: Enum?
+    result: types.Byte
+
+@public
+class TribeSettingsPacket(ClientboundTribullePacket):
+    id = 130
+
+    class MemberInfo(pak.SubPacket):
+        class LocationInfo(pak.SubPacket):
+            service_id: types.Int
+            room_name:  types.SignedLengthString
+
+        global_id: types.Int
+        username:  types.SignedLengthString
+
+        gender:     pak.Enum(types.Byte, enums.Gender)
+        avatar_id:  types.Int
+        last_login: types.Int
+        rank_index: types.Byte
+
+        location: LocationInfo
+
+    class RankInfo(pak.SubPacket):
+        name:        types.SignedLengthString
+        permissions: types.Int
+
+    tribe_id:       types.Int
+    tribe_name:     types.SignedLengthString
+    greeting:       types.SignedLengthString
+    house_map_code: types.Int
+
+    members: MemberInfo[types.Short]
+    ranks:   RankInfo[types.Short]
--- caseus/packets/serverbound/tribulle.py
+++ caseus/packets/serverbound/tribulle.py
@@ -9,3 +9,9 @@ class ChangeTribeHouseMapPacket(ServerboundTribullePacket):
     id = 102
 
     map_code: types.Int
+
+@public
+class GetTribeSettingsPacket(ServerboundTribullePacket):
+    id = 108
+
+    include_disconnected_members: types.ByteBoolean
